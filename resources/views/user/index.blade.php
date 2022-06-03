031c26b812ccc22af8d6b0c1abd213bceac265a9
syokaturyou <teamsumagaku2@gmail.com>
2022-06-03 03:17:40 +0000

    テンプレを用意

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,23 +1,22 @@
-<html>
-  <head>
-　  <title>ユーザー</title>
-　</head>
-　<body>
-　  <h1>ユーザー情報です</h1>
-　  
-　  <table>
-　    <th>id</th>
-　    <th>名前</th>
-　    <th>メールアドレス</th>
-　    <th>パスワード</th>
-      @foreach($users as $user)
+@extends('layouts.master_bootstrap')
+@section('title', 'ユーザー一覧')
+@section('stylesheet')
+<link rel="stylesheet" href="css/toiawase.css"/>
+@endsection
+@section('content')
+  <h1>ユーザー情報です</h1>
+　<table>
+　  <th>id</th>
+　  <th>名前</th>
+　  <th>メールアドレス</th>
+　  <th>パスワード</th>
+    @foreach($users as $user)
       <tr>
         <td>{{$user->id}}</td>
         <td>{{$user->name}}</td>
         <td>{{$user->email}}</td>
         <td>{{$user->password}}</td>
       </tr>
-      @endforeach
-   </table>
-　</body>
-</html>
+    @endforeach
+  </table>
+@endsection
