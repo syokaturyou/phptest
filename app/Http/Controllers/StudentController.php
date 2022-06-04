b19564cf4fbfc808330c2ba1905c2e6c12d75d2e
syokaturyou <teamsumagaku2@gmail.com>
2022-06-04 02:58:54 +0000

    crud処理途中

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+// use Illuminate\Http\Request;
 use App\Models\Student;
 
 class StudentController extends Controller
