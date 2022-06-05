238f77ad375b5d1c8b5bb41f2603e76768d339fd
syokaturyou <teamsumagaku2@gmail.com>
2022-06-05 08:27:32 +0000

    改修

--- resources/views/contents/list.blade.php
+++ resources/views/contents/list.blade.php
@@ -6,14 +6,13 @@
 @section('content')
 <h1>投稿内容</h1>
 <ul>
-    <li><a href="{{ route('contents.create') }}">新規投稿</a></li>
+  <li><a href="{{ route('contents.create') }}">新規投稿</a></li>
 </ul>
 @foreach ($content_infos as $content_info)
-    <div>{{ $content_info->content }}</div>
-    <div>{{ $content_info->updated_at }}</div>
-    <a href="{{ route('contents.update', ['content_id' => $content_info->id]) }}">編集</a>
-    <a href="{{ route('contents.delete', ['content_id' => $content_info->id]) }}">削除</a>
-    <br>
-    <br>
+  <div>{{ $content_info->content }}</div>
+  <div>{{ $content_info->updated_at }}</div>
+  <a href="{{ route('contents.update', ['content_id' => $content_info->id]) }}">編集</a>
+  <a href="{{ route('contents.delete', ['content_id' => $content_info->id]) }}">削除</a>
+  <br>
 @endforeach
 @endsection
--- resources/views/contents/update.blade.php
+++ resources/views/contents/update.blade.php
@@ -5,14 +5,14 @@
 @endsection
 @section('content')
 <h1>編集</h1>
-        <form action="{{ route('contents.save') }}" method="post">
-            @csrf
-            @error('content')
-                {{ $message }}
-                <br>
-            @enderror
-            <input type="hidden" name="id" value="{{ $content_info->id }}">
-            <textarea name="content"  cols="30" rows="10">{{ $content_info->content }}</textarea>
-            <input type="submit" value="保存">
-        </form>
+  <form action="{{ route('contents.save') }}" method="post">
+    @csrf
+    @error('content')
+      {{ $message }}
+      <br>
+    @enderror
+    <input type="hidden" name="id" value="{{ $content_info->id }}">
+      <textarea name="content"  cols="30" rows="10">{{ $content_info->content }}</textarea>
+    <input type="submit" value="保存">
+  </form>
 @endsection
