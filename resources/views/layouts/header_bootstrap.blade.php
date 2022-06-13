b5550ba0d7e5f8f651460928747fe6c955b88a2e
syokaturyou <teamsumagaku2@gmail.com>
2022-06-13 02:14:12 +0000

    インデント整理、不要記述修正

--- resources/views/layouts/header_bootstrap.blade.php
+++ resources/views/layouts/header_bootstrap.blade.php
@@ -2,26 +2,16 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary static-top">
   <div class="container">
     <a class="navbar-brand" href="">
-        <img src="{{ asset('/assets/images/bootstrap_tmp/logo.svg') }}" alt="Laravel学習用">
+      <a class="is-imghover" href="/"><img src="https://popchat.jp/wp-content/themes/wp-popchat/assets/images/common/logo.png" alt="POPCHAT Change the future by the POPCHAT@Cloud"></a>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     
-    <div class="signin">
-      <form action="" method="POST">
-        <label for="signin-id">アカウント名</label>
-        <input id="signin-id" name="name" type="text" placeholder="メールアドレスを入力">
-        <label for="signin-pass">パスワード</label>
-        <input id="signin-pass" name="password" type="text" placeholder="パスワードを入力">
-        <button name="signin" type="submit">ログインする</button>
-      </form>
-    </div>
-    
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="">Home
+          <a class="nav-link" href="/">Home
             <span class="sr-only">(current)</span>
           </a>
         </li>
